bfe3afb1d990dc6cee6db731ca2781ec84f34b11
Restyled.io <commits@restyled.io>
2024-06-22 00:12:15 +0000

    Restyled by clang-format

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,23 +1,23 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-  prefix: "/service/",
-  bare: "/bare/",
+  prefix : "/service/",
+  bare : "/bare/",
   // Replace these lines
-  encodeUrl: function(url) {
+  encodeUrl : function(url) {
     // Remove leading https:// if present
     url = url.replace(/^https?:\/\/+/, "");
     return url;
   },
-  decodeUrl: function(url) {
+  decodeUrl : function(url) {
     // Add https:// if not present
     if (!url.startsWith("https://")) {
       url = "https://" + url;
     }
     return url;
   },
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+  handler : "/uv/uv.handler.js",
+  client : "/uv/uv.client.js",
+  bundle : "/uv/uv.bundle.js",
+  config : "/uv/uv.config.js",
+  sw : "/uv/uv.sw.js",
 };
