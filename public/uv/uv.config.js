c8be2b462fab09cf063f8b36188ecbaa470aecf3
Damian Swan <damianmswan@icloud.com>
2024-06-21 20:12:08 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,20 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-  prefix: "/u/",
+  prefix: "/service/",
   bare: "/bare/",
+  // Replace these lines
+  encodeUrl: function(url) {
+    // Remove leading https:// if present
+    url = url.replace(/^https?:\/\/+/, "");
+    return url;
+  },
+  decodeUrl: function(url) {
+    // Add https:// if not present
+    if (!url.startsWith("https://")) {
+      url = "https://" + url;
+    }
+    return url;
+  },
   handler: "/uv/uv.handler.js",
   client: "/uv/uv.client.js",
   bundle: "/uv/uv.bundle.js",
