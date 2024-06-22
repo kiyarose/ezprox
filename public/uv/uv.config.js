49d8423c00807f8fd86ca686a66ef28b613f9216
Damian Swan <damianmswan@icloud.com>
2024-06-21 20:21:36 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,19 +2,6 @@
 self.__uv$config = {
   prefix: "/service/",
   bare: "/bare/",
-  // Replace these lines
-  encodeUrl: function(url) {
-    // Remove leading https:// if present
-    url = url.replace(/^https?:\/\/+/, "");
-    return url;
-  },
-  decodeUrl: function(url) {
-    // Add https:// if not present
-    if (!url.startsWith("https://")) {
-      url = "https://" + url;
-    }
-    return url;
-  },
   handler: "/uv/uv.handler.js",
   client: "/uv/uv.client.js",
   bundle: "/uv/uv.bundle.js",
