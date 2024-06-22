a75ceed4d7b93c3ce52499afe3db3e34eaba4b44
Restyled.io <commits@restyled.io>
2024-06-22 00:35:44 +0000

    Restyled by clang-format

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,10 +1,10 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-  prefix: "/u/",
-  bare: "/bare/",
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+  prefix : "/u/",
+  bare : "/bare/",
+  handler : "/uv/uv.handler.js",
+  client : "/uv/uv.client.js",
+  bundle : "/uv/uv.bundle.js",
+  config : "/uv/uv.config.js",
+  sw : "/uv/uv.sw.js",
 };
